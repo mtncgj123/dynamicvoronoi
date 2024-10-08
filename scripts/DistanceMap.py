3e2f59941a57c87627d27e4701a0337de7373ed8
zhoumiaojun <zhoumiaojun@move-new.com>
2024-10-08 16:07:54 +0800

    modify raise by using divider

--- scripts/DistanceMap.py
+++ scripts/DistanceMap.py
@@ -7,6 +7,8 @@ dist = 99999.0*np.ones((30, 30))
 obst = -1*np.ones((30, 30))
 toRaise = np.zeros((30, 30))
 sqrt_map = np.zeros((30, 30))
+divider = []
+divider_map = -1*np.ones((30, 30))
 count = 0
 pop_index = 0  # 0 for pop front.-1 for pop back
 
@@ -65,6 +67,25 @@ def Raise(i, j):
                 sorted_distance_list.add([xi, xj])
     toRaise[i, j] = 0
 
+# def RemoveObstacle(i, j):
+#     # obst[i, j] = -1
+#     toRaise[i, j] = 1
+#     # dist[i, j] = 99999.0
+#     sorted_distance_list.add([i, j])
+
+# def Raise(i, j):
+#     obs_index = obst[i, j]
+#     obs_i, obs_j = S2IJ(obs_index)
+#     if i == obs_i and j == obs_j:
+#         for index_i in range(30):
+#             for index_j in range(30):
+#                 if obst[index_i, index_j] == obs_index:
+#                     dist[index_i, index_j] = 99999.0
+#                     obst[index_i, index_j] = -1
+#                 if divider_map[index_i, index_j] == obs_index:
+#                     sorted_distance_list.add([index_i, index_j])
+#     toRaise[i, j] = 0
+
 
 def Lower(i, j):
     si, sj = S2IJ(obst[i, j])
@@ -77,13 +98,17 @@ def Lower(i, j):
             xj = j + nj
             if xi < 0 or xi >= 30 or xj < 0 or xj >= 30:
                 continue
-            if toRaise[xi, xj] == 0:
+            # if toRaise[xi, xj] == 0:
+            if True:
                 zl_si, zl_sj = S2IJ(obst[si, sj])
                 d = sqrt_map[int(abs(si-xi)), int(abs(sj-xj))]
                 if ((d < dist[xi, xj]) or (obst[xi, xj] < 0)) and (zl_si == si) and (zl_sj == sj):
                     dist[xi, xj] = d
                     obst[xi, xj] = si*30+sj
                     sorted_distance_list.add([xi, xj])
+                if (dist[xi, xj] < d):
+                    divider_map[xi, xj] = obst[i, j]
+                    divider_map[i, j] = obst[xi, xj]
 
 
 def Pops():  # get the element with max distance
@@ -116,6 +141,18 @@ def UpdateDistanceMap():
         # plt.colorbar()  # 添加颜色条以便于理解距离值
         # plt.pause(0.001)  # 暂停以便查看更新的图像
 
+    # for i in range(30):
+    #     for j in range(30):
+    #         for ni in [-1, 0, 1]:
+    #             for nj in [-1, 0, 1]:
+    #                 xi = i + ni
+    #                 xj = j + nj
+    #                 if xi < 0 or xi >= 30 or xj < 0 or xj >= 30:
+    #                     continue
+    #                 if (obst[xi, xj] != obst[i, j]):
+    #                     divider.append([i, j])
+    #                     divider.append([xi, xj])
+
 
 plt.ion()  # 开启交互模式
 StartTime = time.time()
@@ -133,8 +170,15 @@ UpdateDistanceMap()
 RemoveObstacle(15, 15)
 RemoveObstacle(14, 16)
 RemoveObstacle(25, 25)
+RemoveObstacle(16, 16)
+RemoveObstacle(15, 16)
+RemoveObstacle(5, 5)
 UpdateDistanceMap()
 SetObstacle(25, 28)
+SetObstacle(10, 28)
+UpdateDistanceMap()
+RemoveObstacle(10, 28)
+RemoveObstacle(10, 25)
 UpdateDistanceMap()
 
 
