c92c2a0f19b6ece717782f203e78e46442b2ae74
mtncgjb123 <nuaazora123@gmail.com>
2024-11-01 16:57:48 +0800

    DistanceMap add voronoi generation

--- scripts/DistanceMap.py
+++ scripts/DistanceMap.py
@@ -2,19 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from sortedcontainers import SortedList
-open_list = []
 dist = 99999.0*np.ones((30, 30))
 obst = -1*np.ones((30, 30))
 toRaise = np.zeros((30, 30))
 sqrt_map = np.zeros((30, 30))
-divider = []
 divider_map = -1*np.ones((30, 30))
 voronoi_map = np.zeros((30, 30))
 pop_index = 0  # 0 for pop front.-1 for pop back
+isAnimationOn = False
 
+# 生成距离表格
 for i in range(30):
     for j in range(30):
         sqrt_map[i, j] = np.sqrt(i**2 + j**2)
+
+
+class Voronoi:
+    voronoiKeep = -4
+    freeQueued = -3
+    voronoiRetry = -2
+    voronoiPrune = -1
+    free = 0
+    occupied = 1
+
+
+class CmarkerMatchResult:
+    pruned = 0
+    keep = 1
+    retry = 2
 
 
 def distance_key(List_Element):
@@ -36,50 +51,24 @@ def S2IJ(s):
 def SetObstacle(i, j):
     obst[i, j] = i*30+j
     dist[i, j] = 0
-    voronoi_map[i, j] = 1
+    voronoi_map[i, j] = Voronoi.occupied
     sorted_distance_list.add([i, j])
 
-
-# def RemoveObstacle(i, j):
-#     obst[i, j] = -1
-#     toRaise[i, j] = 1
-#     dist[i, j] = 99999.0
-#     sorted_distance_list.add([i, j])
-
-
-# def Raise(i, j):
-#     for ni in [-1, 0, 1]:
-#         for nj in [-1, 0, 1]:
-#             if ni == 0 and nj == 0:
-#                 continue
-
-#             xi = i + ni
-#             xj = j + nj
-#             if xi < 0 or xi >= 30 or xj < 0 or xj >= 30:
-#                 continue
-
-#             if (obst[xi, xj] >= 0) and (toRaise[xi, xj] == 0):
-#                 oi, oj = S2IJ(obst[xi, xj])
-#                 zl_oi, zl_oj = S2IJ(obst[oi, oj])
-#                 if (obst[oi, oj] == -1 or (zl_oi != oi) or (zl_oj != oj)):
-#                     dist[xi, xj] = 99999.0
-#                     obst[xi, xj] = -1
-#                     toRaise[xi, xj] = 1
-
-#                 sorted_distance_list.add([xi, xj])
-#     toRaise[i, j] = 0
 
 def RemoveObstacle(i, j):
-    # obst[i, j] = -1
     toRaise[i, j] = 1
-    # dist[i, j] = 99999.0
     sorted_distance_list.add([i, j])
+
+
+def isOccupied(i, j):
+    obs_index = obst[i, j]
+    obs_i, obs_j = S2IJ(obs_index)
+    return (i == obs_i and j == obs_j)
 
 
 def Raise(i, j):
     obs_index = obst[i, j]
-    obs_i, obs_j = S2IJ(obs_index)
-    if i == obs_i and j == obs_j:
+    if isOccupied(i, j):
         for index_i in range(30):
             for index_j in range(30):
                 if obst[index_i, index_j] == obs_index:
@@ -91,7 +80,7 @@ def Raise(i, j):
 
 
 def Lower(i, j):
-    voronoi_map[i, j] = 1
+    voronoi_map[i, j] = Voronoi.occupied
     si, sj = S2IJ(obst[i, j])
     for ni in [-1, 0, 1]:
         for nj in [-1, 0, 1]:
@@ -100,20 +89,18 @@ def Lower(i, j):
 
             xi = i + ni
             xj = j + nj
-            if xi < 0 or xi >= 30 or xj < 0 or xj >= 30:
+            if xi <= 0 or xi >= 30-1 or xj <= 0 or xj >= 30-1:
                 continue
-            # if toRaise[xi, xj] == 0:
-            if True:
-                zl_si, zl_sj = S2IJ(obst[si, sj])
-                d = sqrt_map[int(abs(si-xi)), int(abs(sj-xj))]
-                if ((d < dist[xi, xj]) or (obst[xi, xj] < 0)) and (zl_si == si) and (zl_sj == sj):
-                    dist[xi, xj] = d
-                    obst[xi, xj] = si*30+sj
-                    sorted_distance_list.add([xi, xj])
-                if (dist[xi, xj] < d):
-                    divider_map[xi, xj] = obst[i, j]
-                    divider_map[i, j] = obst[xi, xj]
-                    checkVoronoi(xi, xj, i, j)
+
+            d = sqrt_map[abs(si-xi), abs(sj-xj)]
+            if ((d < dist[xi, xj]) or (obst[xi, xj] < 0)) and isOccupied(si, sj):
+                dist[xi, xj] = d
+                obst[xi, xj] = si*30+sj
+                sorted_distance_list.add([xi, xj])
+            if (dist[xi, xj] < d):
+                divider_map[xi, xj] = obst[i, j]
+                divider_map[i, j] = obst[xi, xj]
+                checkVoronoi(xi, xj, i, j)
 
 
 def Pops():  # get the element with max distance
@@ -135,40 +122,26 @@ def UpdateDistanceMap():
         i, j = S2IJ(s)
         if toRaise[i, j] == 1:
             Raise(i, j)
-            # print("raise")
         else:
             if obst[i, j] >= 0:
                 Lower(i, j)
-                # print("lower")
         not_empty, s = Pops()
-        # plt.clf()  # 清除当前图形，不再叠加图像
-        # plt.imshow(dist, cmap='viridis', vmin=0, vmax=25)  # 可以设置不同的 colormap
-        # plt.colorbar()  # 添加颜色条以便于理解距离值
-        # plt.pause(0.001)  # 暂停以便查看更新的图像
-
-    # for i in range(30):
-    #     for j in range(30):
-    #         for ni in [-1, 0, 1]:
-    #             for nj in [-1, 0, 1]:
-    #                 xi = i + ni
-    #                 xj = j + nj
-    #                 if xi < 0 or xi >= 30 or xj < 0 or xj >= 30:
-    #                     continue
-    #                 if (obst[xi, xj] != obst[i, j]):
-    #                     divider.append([i, j])
-    #                     divider.append([xi, xj])
+        if isAnimationOn:
+            plt.clf()
+            plt.imshow(dist, cmap='viridis', vmin=0, vmax=25)
+            plt.pause(0.001)
 
 
 def ShowDivider():
     for i in range(30):
         for j in range(30):
             if isVoronoi(i, j):
-                # dist[i, j] = 0
-                plt.gca().add_patch(plt.Rectangle((j, i), 1, 1, color='red'))
+                dist[i, j] = 0
+                plt.gca().add_patch(plt.Rectangle((j-0.5, i-0.5), 1, 1, color='red'))
 
 
 def isVoronoi(i, j):
-    return voronoi_map[i, j] == 0 or voronoi_map[i, j] == -4
+    return voronoi_map[i, j] == Voronoi.free or voronoi_map[i, j] == Voronoi.voronoiKeep
 
 
 def checkVoronoi(x, y, nx, ny):
@@ -176,7 +149,7 @@ def checkVoronoi(x, y, nx, ny):
     obsxy_i, obsxy_j = S2IJ(obsxy)
     obsn = obst[nx, ny]
     obsn_i, obsn_j = S2IJ(obsn)
-    if (dist[x, y] > 1 or dist[nx, ny] > 1) and dist[nx, ny] >= 0:
+    if (dist[x, y] > 1 or dist[nx, ny] > 1):
         if (abs(obsxy_i-obsn_i) > 1 or abs(obsxy_j-obsn_j) > 1):
             dxy_x = abs(x-obsn_i)
             dxy_y = abs(y-obsn_j)
@@ -188,16 +161,15 @@ def checkVoronoi(x, y, nx, ny):
             dnxy_y = abs(ny-obsxy_j)
             sqdnxy = sqrt_map[dnxy_x, dnxy_y]
             stability_nxy = sqdnxy-dist[nx, ny]
-            if stability_nxy < 0:
-                return
+
             if stability_xy <= stability_nxy and dist[x, y] > 2:
-                if (voronoi_map[x, y] != 0):
-                    voronoi_map[x, y] = 0
+                if (voronoi_map[x, y] != Voronoi.free):
+                    voronoi_map[x, y] = Voronoi.free
                     pruneQueue.add([x, y])
 
             if stability_nxy <= stability_xy and dist[nx, ny] > 2:
-                if (voronoi_map[nx, ny] != 0):
-                    voronoi_map[nx, ny] = 0
+                if (voronoi_map[nx, ny] != Voronoi.free):
+                    voronoi_map[nx, ny] = Voronoi.free
                     pruneQueue.add([nx, ny])
 
 
@@ -206,27 +178,27 @@ def prune():
         max_element = pruneQueue.pop(pop_index)
         x = max_element[0]
         y = max_element[1]
-        voronoi_map[x, y] = -3
+        voronoi_map[x, y] = Voronoi.freeQueued
         sorted_distance_list.add([x, y])
 
-        if ((x+2 < 30) and voronoi_map[x+1, y] == 1 and (y+1) < 30 and (y-1) >= 0):
-            if (voronoi_map[x+1, y+1] != 1 and voronoi_map[x+1, y-1] != 1 and voronoi_map[x+2, y] != 1):
-                voronoi_map[x+1, y] = -3
+        if ((x+2 < 30) and voronoi_map[x+1, y] == Voronoi.occupied):
+            if (voronoi_map[x+1, y+1] != Voronoi.occupied and voronoi_map[x+1, y-1] != Voronoi.occupied and voronoi_map[x+2, y] != Voronoi.occupied):
+                voronoi_map[x+1, y] = Voronoi.freeQueued
                 sorted_distance_list.add([x+1, y])
 
-        if ((x-2 >= 0) and voronoi_map[x-1, y] == 1 and (y+1) < 30 and (y-1) >= 0):
-            if (voronoi_map[x+1, y+1] != 1 and voronoi_map[x-1, y-1] != 1 and voronoi_map[x-2, y] != 1):
-                voronoi_map[x-1, y] = -3
+        if ((x-2 >= 0) and voronoi_map[x-1, y] == Voronoi.occupied):
+            if (voronoi_map[x-1, y+1] != Voronoi.occupied and voronoi_map[x-1, y-1] != Voronoi.occupied and voronoi_map[x-2, y] != Voronoi.occupied):
+                voronoi_map[x-1, y] = Voronoi.freeQueued
                 sorted_distance_list.add([x-1, y])
 
-        if ((y-2 >= 0) and voronoi_map[x, y-1] == 1 and (x-1) >= 0 and (x+1) < 30):
-            if (voronoi_map[x+1, y-1] != 1 and voronoi_map[x-1, y-1] != 1 and voronoi_map[x, y-2] != 1):
-                voronoi_map[x, y-1] = -3
+        if ((y-2 >= 0) and voronoi_map[x, y-1] == Voronoi.occupied):
+            if (voronoi_map[x+1, y-1] != Voronoi.occupied and voronoi_map[x-1, y-1] != Voronoi.occupied and voronoi_map[x, y-2] != Voronoi.occupied):
+                voronoi_map[x, y-1] = Voronoi.freeQueued
                 sorted_distance_list.add([x, y-1])
 
-        if ((y+2 < 30) and voronoi_map[x, y+1] == 1 and (x-1) >= 0 and (x+1) < 30):
-            if (voronoi_map[x+1, y+1] != 1 and voronoi_map[x-1, y+1] != 1 and voronoi_map[x, y+2] != 1):
-                voronoi_map[x, y+1] = -3
+        if ((y+2 < 30) and voronoi_map[x, y+1] == Voronoi.occupied):
+            if (voronoi_map[x+1, y+1] != Voronoi.occupied and voronoi_map[x-1, y+1] != Voronoi.occupied and voronoi_map[x, y+2] != Voronoi.occupied):
+                voronoi_map[x, y+1] = Voronoi.freeQueued
                 sorted_distance_list.add([x, y+1])
 
     while (len(sorted_distance_list) > 0):
@@ -234,18 +206,18 @@ def prune():
         x = max_element[0]
         y = max_element[1]
         v = voronoi_map[x, y]
-        if (v != - 3 and v != -2):
+        if (v != Voronoi.freeQueued and v != Voronoi.voronoiRetry):
             continue
         markerMatchResult = markerMatch(x, y)
-        if (markerMatchResult == 0):
-            voronoi_map[x, y] = -1
-        elif (markerMatchResult == 1):
-            voronoi_map[x, y] = -4
+        if (markerMatchResult == CmarkerMatchResult.pruned):
+            voronoi_map[x, y] = Voronoi.voronoiPrune
+        elif (markerMatchResult == CmarkerMatchResult.keep):
+            voronoi_map[x, y] = Voronoi.voronoiKeep
         else:
-            voronoi_map[x, y] = -2
+            voronoi_map[x, y] = Voronoi.voronoiRetry
             pruneQueue.add([x, y])
 
-        if (len(sorted_distance_list) > 0):
+        if (len(sorted_distance_list) == 0):
             while (len(pruneQueue) > 0):
                 p = pruneQueue.pop(pop_index)
                 sorted_distance_list.add([p[0], p[1]])
@@ -261,54 +233,73 @@ def markerMatch(x, y):
             if (dx or dy):
                 nx = x+dx
                 ny = y+dy
-                if (nx >= 0 and nx < 30 and ny >= 0 and ny < 30):
-                    v = voronoi_map[nx, ny]
-                    b = (v <= 0 and v != -1)
-                    f[i] = b
-                    if (b):
-                        voroCount += 1
-                        if (not (dx and dy)):
-                            voroCountFour += 1
-                    i += 1
+                v = voronoi_map[nx, ny]
+                b = (v <= Voronoi.free and v != Voronoi.voronoiPrune)
+                f[i] = b
+                if (b):
+                    voroCount += 1
+                    if (not (dx and dy)):
+                        voroCountFour += 1
+                i += 1
     if (voroCount < 3 and voroCountFour == 1):
-        return 1
+        return CmarkerMatchResult.keep
 
     if (((not f[0]) and f[1] and f[3]) or ((not f[2]) and f[1] and f[4]) or ((not f[5]) and f[3] and f[6]) or ((not f[7]) and f[6] and f[4])):
-        return 1
-    if ((f[3] and f[4] and (not f[1] and (not f[6]))) or (f[1] and f[6] and (not f[3] and (not f[4])))):
-        return 1
+        return CmarkerMatchResult.keep
+    if ((f[3] and f[4] and (not f[1]) and (not f[6])) or (f[1] and f[6] and (not f[3]) and (not f[4]))):
+        return CmarkerMatchResult.keep
 
-    if (voroCount >= 0 and voroCountFour >= 3 and voronoi_map[x, y] != -2):
-        return 2
+    if (voroCount >= 5 and voroCountFour >= 3 and voronoi_map[x, y] != Voronoi.voronoiRetry):
+        return CmarkerMatchResult.retry
 
-    return 0
+    return CmarkerMatchResult.pruned
 
 
-plt.ion()  # 开启交互模式
+plt.ion()
 StartTime = time.time()
-SetObstacle(15, 15)
-SetObstacle(16, 15)
-SetObstacle(14, 15)
-SetObstacle(14, 16)
-SetObstacle(15, 16)
-SetObstacle(16, 16)
-SetObstacle(25, 15)
-SetObstacle(10, 25)
-SetObstacle(25, 25)
-SetObstacle(5, 5)
-UpdateDistanceMap()
-RemoveObstacle(15, 15)
-RemoveObstacle(14, 16)
-RemoveObstacle(25, 25)
-RemoveObstacle(16, 16)
-RemoveObstacle(15, 16)
-RemoveObstacle(5, 5)
-UpdateDistanceMap()
-SetObstacle(25, 28)
-SetObstacle(10, 28)
-UpdateDistanceMap()
-RemoveObstacle(10, 28)
-RemoveObstacle(10, 25)
+SetObstacle(20, 20)
+SetObstacle(19, 20)
+SetObstacle(18, 20)
+SetObstacle(17, 20)
+SetObstacle(16, 20)
+SetObstacle(15, 20)
+SetObstacle(14, 20)
+SetObstacle(13, 20)
+SetObstacle(12, 20)
+SetObstacle(11, 20)
+SetObstacle(10, 20)
+SetObstacle(9, 20)
+SetObstacle(8, 20)
+SetObstacle(7, 20)
+SetObstacle(6, 20)
+SetObstacle(5, 20)
+SetObstacle(5, 19)
+SetObstacle(5, 18)
+SetObstacle(5, 17)
+SetObstacle(5, 16)
+SetObstacle(5, 15)
+SetObstacle(5, 14)
+SetObstacle(5, 13)
+SetObstacle(5, 12)
+SetObstacle(5, 11)
+SetObstacle(5, 10)
+SetObstacle(5, 9)
+SetObstacle(5, 8)
+SetObstacle(6, 8)
+SetObstacle(7, 8)
+SetObstacle(8, 8)
+SetObstacle(9, 8)
+SetObstacle(10, 8)
+SetObstacle(11, 8)
+SetObstacle(12, 8)
+SetObstacle(13, 8)
+SetObstacle(14, 8)
+SetObstacle(15, 8)
+SetObstacle(16, 8)
+SetObstacle(17, 8)
+SetObstacle(18, 8)
+SetObstacle(19, 8)
+SetObstacle(20, 8)
 UpdateDistanceMap()
 prune()
 ShowDivider()
@@ -318,7 +309,6 @@ print("Time:", time.time() - StartTime, "seconds")
 
 plt.imshow(dist)
 
-
 plt.show()
 plt.colorbar()
-plt.pause(10)
+plt.pause(5)
